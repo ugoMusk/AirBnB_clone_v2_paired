850b7e9fdeb020ec191b270d80982869ed46ef9b
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-08-23 03:03:35 -0700

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
--- create database hbnb_dev_db
--- create user hbnb_dev with pwd hbnb_dev_pwd
+-- create database hbnb_dev_db if it does not exits
+-- create user hbnb_dev with pwd hbnb_dev_pwd if it not exits
 -- grant hbnb_dev select on performance_schema
 -- grant all privileges on hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
