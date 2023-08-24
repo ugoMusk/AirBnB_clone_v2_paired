1296452808686fa2c67e13e4eae2ec62bbdeae86
ugoMusk <ugobothankgod@gmail.com>
2023-08-24 03:55:40 +0100

    update git_auto_push

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 echo git_auto_push.sh >> ../.gitignore
-./.gitignore.s
 list=`ls $(pwd)`
 
 for i in $list;
-do n=$(grep -oE "^|\.[a-z]{1,5}$" <<< $i) &&
-        bn=$(basename -s "$n" $i) &&
-        git add $i && git commit -m "update $bn" && git push &&;
+do n=$(grep -oE "^|\.[a-z]{1,5}$" <<< $i) && bn=$(basename -s "$n" $i) && git add $i && git commit -m "update $bn" && git push;
 done
