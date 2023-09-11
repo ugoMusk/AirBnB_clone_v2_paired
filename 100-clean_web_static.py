dc75249e2351c47570adebdd5f0f3fecda075b8e
ugoMusk <ugobothankgod@gmail.com>
2023-09-11 05:06:58 +0000

    send updates

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,59 +1,29 @@
 #!/usr/bin/python3
-"""script (based on the file 3-deploy_web_static.py) that deletes
-out-of-date archives, using the function do_clean:"""
-from fabric.api import local, env, put, run
-from time import strftime
-import os.path
-env.hosts = ['52.91.153.40', '54.84.51.64']
+import os
+from fabric.api import *
 
-def do_pack():
-    """deploy the server"""
-    timenow = strftime("%Y%M%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        filename = "versions/web_static_{}.tgz".format(timenow)
-        local("tar -cvzf {} web_static/".format(filename))
-        return filename
-    except:
-        return None
-
-def do_deploy(archive_path):
-    """do deploy"""
-    if os.path.isfile(archive_path) is False:
-        return False
-    try:
-        filename = archive_path.split("/")[-1]
-        no_ext = filename.split(".")[0]
-        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
-        symlink = "/data/web_static/current"
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(path_no_ext))
-        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
-        run("rm -rf {}web_static".format(path_no_ext))
-        run("rm -rf {}".format(symlink))
-        run("ln -s {} {}".format(path_no_ext, symlink))
-        return True
-    except:
-        return False
-
-def deploy():
-    """deploy"""
-    archive_path = do_pack()
-    if archive_path is None:
-        return False
-    success = do_deploy(archive_path)
-    return success
+env.hosts = ['100.25.19.204', '54.157.159.85']
 
 
 def do_clean(number=0):
-    """clean"""
-    if number == 0:
-        number = 1
-    with cd.local('./versions'):
-            local("ls -lt | tail -n +{} | rev | cut -f1 -d" " | rev | \
-            xargs -d '\n' rm".format(1 + number))
-    with cd('/data/web_static/releases/'):
-            run("ls -lt | tail -n +{} | rev | cut -f1 -d" " | rev | \
-            xargs -d '\n' rm".format(1 + number))
+    """Delete out-of-date archives.
+
+    Args:
+        number (int): The number of archives to keep.
+
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
