41aa3c35dcae1688d231ac9d183b64f30645c65d
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-09-22 09:27:58 -0700

    Python is cool!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """Returns text"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello():
+    """Return text"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """replace text with variable"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
