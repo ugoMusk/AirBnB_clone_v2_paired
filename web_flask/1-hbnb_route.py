6d537e6c19b5262be8c9e18e0801ef5c5f648aad
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-09-22 09:11:48 -0700

    updated

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,8 +9,14 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
-    return "Hello HBNB!"
+    """Returns text"""
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """Return text"""
+    return 'HBNB'
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
