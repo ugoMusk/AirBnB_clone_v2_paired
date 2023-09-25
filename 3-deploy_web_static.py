157a4bf0db4ad93c4d19bd73bd66543a5e8ccf6e
ugoMusk <ugobothankgod@gmail.com>
2023-09-25 01:06:49 +0000

    deploy main directory, feels cool

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.234.9.227', '18.207.236.151']
 
 
 def do_pack():
